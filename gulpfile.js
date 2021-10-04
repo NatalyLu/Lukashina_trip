c293ba4438a7d6aa7c57bf9f92b5140ea81d0a91
Natalia <lukashina_natalia@mil.ru>
2021-10-04 23:59:07 +0300

    corrected gulp comands and created json files

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,27 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const csso = require("postcss-csso");
+const htmlmin = require("gulp-htmlmin");
+const terser = require("gulp-terser-js");
 const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
+const avif = require('gulp-avif');
 const svgstore = require("gulp-svgstore");
-const htmlmin = require("gulp-htmlmin");
-const terser = require("gulp-terser-js");
-const sync = require("browser-sync").create();
-const del= require("del");
 const concat = require("gulp-concat");
 const order = require("gulp-order");
-const avif = require('gulp-avif');
+const sync = require("browser-sync").create();
+const del= require("del");
 
 // HTML
-
 const html = () => {
   return gulp.src("source/**/*.html")
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest("build"));
 };
-
 exports.html = html;
 
 // Styles
-
 const styles = () => {
   return gulp.src("source/less/style.less")
   .pipe(plumber())
@@ -42,11 +39,9 @@ const styles = () => {
   .pipe(gulp.dest("build/css"))
   .pipe(sync.stream());
 };
-
 exports.styles = styles;
 
 // Scripts
-
 const scripts = () => {
   return gulp.src("source/js/*.js")
     .pipe(order([
@@ -67,11 +62,9 @@ const scripts = () => {
     .pipe(gulp.dest("build/js"))
     .pipe(sync.stream());
 }
-
 exports.scripts = scripts;
 
 // Images
-
 const optimizeImages = () => {
   return gulp.src("source/img/**/*.{jpg,png,svg}")
   .pipe(imagemin([
@@ -81,64 +74,44 @@ const optimizeImages = () => {
   ]))
   .pipe(gulp.dest("build/img"))
 };
-
 exports.optimizeImages = optimizeImages;
 
 // WebP
-
 const createWebp = () => {
   return gulp.src("source/img/**/*.{jpg,png}")
   .pipe(webp({quality: 80}))
   .pipe(gulp.dest("build/img"))
 };
-
 exports.createWebp = createWebp;
 
 // Avif
-
 const createAvif = () => {
   return gulp.src('source/img/**/*.{png,jpg}')
   .pipe(avif({quality: 80}))
   .pipe(gulp.dest('build/img'));
 };
-
 exports.createAvif = createAvif;
 
 // Sprite
-
 const sprite = () => {
-return gulp.src("source/img/sprite/*.svg")
+return gulp.src("source/img/svg/*.svg")
 .pipe(svgstore({
   inlineSvg: true
 }))
 .pipe(rename("sprite.svg"))
 .pipe(gulp.dest("build/img/svg/"));
 };
-
 exports.sprite = sprite;
 
 // Copy
-
 const copy = (done) => {
-  gulp.src([
-    "source/*.{xml,png,ico,svg,webmanifest}",
-    "source/img/**/*.{jpg,png}",
-    "source/img/svg/*.svg",
-    "source/js/jquery/jquery-3.6.0.min.js",
-    "source/js/slick/*.js",
-    "source/phpmailer/*",
-    "source/css/slick.min.css"
-  ], {
-    base: "source"
-  })
+  gulp.src("source/*.{xml,png,ico,svg,webmanifest}")
   .pipe(gulp.dest("build"))
   done();
 };
-
 exports.copy = copy;
 
 // Server
-
 const server = (done) => {
   sync.init({
     server: {
@@ -150,33 +123,33 @@ const server = (done) => {
   });
   done();
 };
-
 exports.server = server;
 
 // Reload
-
 const reload = done => {
   sync.reload();
   done();
 };
 
 // Watcher
-
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series(styles));
   gulp.watch("source/*.html", gulp.series(html, reload));
 };
 
 // Clean
-
 const clean = () => {
   return del("build");
 };
-
 exports.clean = clean;
 
-// Build
+// simpleClean
+const simpleClean = () => {
+  return del(["build/css", "build/js", "build/index.html"]);
+}
+exports.clean = simpleClean;
 
+// Build
 const build = gulp.series(
   clean,
   copy,
@@ -185,26 +158,20 @@ const build = gulp.series(
     styles,
     html,
     sprite,
-    scripts,
+    // scripts,
     createWebp,
     createAvif
   ),
 );
-
 exports.build = build;
 
 // Default
-
 exports.default = gulp.series(
-  clean,
-  copy,
+  simpleClean,
   gulp.parallel(
     styles,
     html,
-    scripts,
-    sprite,
-    createWebp,
-    createAvif
+    // scripts,
   ),
   gulp.series(
     server,
